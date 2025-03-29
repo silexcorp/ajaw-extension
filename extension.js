b2f59f217e0314cd2db748a40b1c26284b7341a9
Alexander Mateo <lex@weareokan.com>
2025-03-29 10:02:03 -0600

    Url fixed

--- extension.js
+++ extension.js
@@ -71,7 +71,7 @@ const AjawIndicator = GObject.registerClass(
             // Developer webpage
             let menuItem = new PopupMenu.PopupMenuItem('Developer');
             menuItem.connect('activate', () => {
-                Gio.AppInfo.launch_default_for_uri('xdg-open http://tech.weareokan.com', null);
+                Gio.AppInfo.launch_default_for_uri('http://tech.weareokan.com', null);
             });
             this.menu.addMenuItem(menuItem);
         }
